587de3a838043c3f1777b2d8665cba314a5af648
Nibha Maharjan <nibhamaharjan@yahoo.com>
2023-11-20 15:48:12 -0500

    Login Screens Made

--- app/src/main/java/com/example/nibhamaharjan_mapd711_assignment4/MainActivity.kt
+++ app/src/main/java/com/example/nibhamaharjan_mapd711_assignment4/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.nibhamaharjan_mapd711_assignment4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cus_regis= findViewById(R.id.textView5) as TextView
+        cus_regis.setOnClickListener {
+            startActivity(Intent(this, CustomerRegister::class.java))
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.login_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent: Intent = when (item.itemId) {
+            R.id.customer_menu -> {
+                Toast.makeText(this, "Welcome Customer", Toast.LENGTH_SHORT).show()
+                Intent(this, MainActivity::class.java)
+            }
+            R.id.admin_menu -> {
+                Toast.makeText(this, "Welcome Admin", Toast.LENGTH_SHORT).show()
+                Intent(this, AdminLogin::class.java)
+            }
+
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        startActivity(intent)
+        return true
     }
 }
